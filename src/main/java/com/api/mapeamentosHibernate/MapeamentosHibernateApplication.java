6a18ba0d3632a4a057f92a1be90bc66e11876a6c
Italo Modesto Pereira <italo.pereira@ccc.ufcg.edu.br>
2022-08-03 16:17:58 -0300

    start funcionando

---
+++ src/main/java/com/api/mapeamentosHibernate/MapeamentosHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.api.mapeamentosHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapeamentosHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapeamentosHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/mapeamentosHibernate/MapeamentosHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.mapeamentosHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapeamentosHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
